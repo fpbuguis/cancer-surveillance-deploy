2dc42607456032aa69e86912aee64709c7a9f109
Francesca Maries P. Buguis <fpbuguis@up.edu.ph>
2024-12-09 01:03:09 +0800

    fix outdir

--- vendor/laravel/jetstream/stubs/inertia/vite.config.js
+++ vendor/laravel/jetstream/stubs/inertia/vite.config.js
@@ -6,15 +6,19 @@ export default defineConfig({
     plugins: [
         laravel({
             input: 'resources/js/app.js',
-            refresh: true,
+            refresh: true, // Enables hot module replacement (HMR)
         }),
         vue({
             template: {
                 transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
+                    base: null, // Base URL for relative asset paths
+                    includeAbsolute: false, // Whether to include absolute asset paths
                 },
             },
         }),
     ],
+    build: {
+        outDir: 'dist', // Specifies the output directory for the build
+        emptyOutDir: true, // Optional: Clears the directory before each build
+    },
 });
